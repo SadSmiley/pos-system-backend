ca417876798adc33cffc63e6b09da84aa784ce9d
Edward Guevarra <edwardguevarra2003@gmail.com>
2023-01-02 11:50:35 +0700

    FIX MONGODB CONNECTION STRING

--- src/db.ts
+++ src/db.ts
@@ -7,13 +7,14 @@ const {
   MONGO_PORT,
   MONGO_DBNAME,
   MONGO_LOCAL,
+  MONGO_CONNECTION_STRING,
 } = process.env;
 
-let MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}?authSource=admin`;
+let MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DBNAME}?authSource=admin`;
 
 if (MONGO_LOCAL) {
   MONGO_URI = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`;
 }
 
-export const client = new MongoClient(MONGO_URI);
+export const client = new MongoClient(MONGO_CONNECTION_STRING || MONGO_URI);
 export const db = client.db();
