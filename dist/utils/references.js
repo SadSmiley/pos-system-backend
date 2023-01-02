16a552a3d8fece610478c06582bb247c18dd01b8
Edward Guevarra <edwardguevarra2003@gmail.com>
2023-01-02 12:19:01 +0700

    Fix Serve

--- dist/utils/references.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.days = void 0;
-exports.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//# sourceMappingURL=references.js.map
